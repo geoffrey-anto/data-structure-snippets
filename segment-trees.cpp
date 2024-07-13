e6ed40066d00ffe5756ed991e073cc0cc9260e99
geoffrey-anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2024-07-14 03:29:30 +0530

    added: seg trees w/o lazy prop

--- segment-trees.cpp
+++ segment-trees.cpp
@@ -2,13 +2,13 @@
 #include <functional>
 using namespace std;
 
-class SegTree {
+class SegTreeLazy {
 public:
   vector<int> seg, lazy;
   function<int(int, int)> updateFunc;
   int n = 0;
 
-  SegTree(function<int(int, int)> u, int n) {
+  SegTreeLazy(function<int(int, int)> u, int n) {
     seg = vector<int>(4 * n, 0);
     lazy = vector<int>(4 * n, 0);
     this->n = n;
@@ -121,5 +121,64 @@ public:
     int rq = queryLazy(2 * i + 2, mid + 1, high, l, r, val, base);
 
     return updateFunc(lq, rq);
+  }
+};
+
+class SegTree {
+public:
+  vector<int> seg;
+  function<int(int, int)> updateFunc;
+  int n = 0;
+
+  SegTree(function<int(int, int)> u, int n) {
+    seg = vector<int>(4 * n, 0);
+    this->n = n;
+    this->updateFunc = u;
+  }
+
+  void build(const vector<int>& arr, int i, int l, int h) {
+    if (l == h) {
+      seg[i] = arr[l];
+      return;
+    }
+
+    int mid = l + (h - l) / 2;
+
+    build(arr, 2 * i + 1, l, mid);
+    build(arr, 2 * i + 2, mid + 1, h);
+
+    seg[i] = updateFunc(seg[2 * i + 1], seg[2 * i + 2]);
+  }
+
+  int query(int i, int l, int h, int ql, int qh, int base = 0) {
+    if (l > qh || h < ql) {
+      return base;
+    }
+
+    if (l >= ql && h <= qh) {
+      return seg[i];
+    }
+
+    int mid = l + (h - l) / 2;
+
+    return updateFunc(query(2 * i + 1, l, mid, ql, qh, base),
+                      query(2 * i + 2, mid + 1, h, ql, qh, base));
+  }
+
+  void update(int i, int l, int h, int idx, int val) {
+    if (l == h) {
+      seg[i] = val;
+      return;
+    }
+
+    int mid = l + (h - l) / 2;
+
+    if (idx <= mid) {
+      update(2 * i + 1, l, mid, idx, val);
+    } else {
+      update(2 * i + 2, mid + 1, h, idx, val);
+    }
+
+    seg[i] = updateFunc(seg[2 * i + 1], seg[2 * i + 2]);
   }
 };
